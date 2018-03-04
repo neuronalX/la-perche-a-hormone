5e8c7187b08a029be219fd71ab1fa598d8d450e9
HINAUT Xavier <xavier.hinaut@inria.fr>
2018-03-04 19:37:18 +0100

    finish complete mechanism

--- mechanisms.py
+++ mechanisms.py
@@ -1,36 +1,94 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-
+# probabilités de mourir
 proba_die = [0,0.2,0.4,0.6]
+# probabilité du pollant de faire changer de sexe (identique pour chaque polluant)
+proba_pol = [0,0.25,0.50,0.75]
+# # # gender id
+# l_genders = ['male', 'intersexe', 'female']
 
-def change_sex_or_die(gender, polluants, depolluants):
+
+def change_sex_or_die(gender, polluants, depolluants, verbose=False):
     """
     Pour chaque poisson on calcule ce qu'il devient :
     - s'il meurt
     - s'il change de sex
 
-    polluants[0] : féminise
-    polluants[1] : masculinise
-    polluants[2] : tue
+    Inputs:
+        - gender : string:
+            'male', 'female'
+        - polluants : [int, int, int]
+            polluants[0] : féminise
+            polluants[1] : masculinise
+            polluants[2] : tue
+        - depolluants
+            dépoluants agissent sur chaque polluant (apparié 1 à 1)
 
-    dépoluants agissent sur chaque polluant (apparié 1 à 1)
+    Outputs :
+        new_sex : string: 'dead', 'male', 'female', 'intersexe'
+
 
     """
     # niveau de polluants
     niv_pol = clean_up(polluants, depolluants)
+    if verbose:
+        print "polluants:", polluants
+        print "niv_pol:", niv_pol
 
     # est-ce que le poisson meurt ?
     if proba_die[niv_pol[2]] > np.random.uniform(0,1):
         return 'dead'
-    # est-ce qu'il change de sexe ?
-    elif 0.5 > np.random.uniform(0,1):
-        if gender == "male":
-            return "female"
-        else:
-            return "male"
+
+    if gender == "male":
+        g_id = 0
+    elif gender == "female":
+        g_id = 2
+
+    # on exécute nr_boucles fois l'action, une fois pour chaque polluant
+    nr_boucles = 2
+    for i in range(nr_boucles):
+        if verbose:
+            print "begin: g_id", g_id
+        # on applique la proba du polluant féminisant
+        if proba_pol[niv_pol[0]] > np.random.uniform(0,1):
+            g_id += 1
+            if verbose:
+                print "after fem: g_id", g_id
+        # on applique la proba du polluant masculinisant
+        if proba_pol[niv_pol[1]] > np.random.uniform(0,1):
+            g_id -= 1
+            if verbose:
+                print "after masc: g_id", g_id
+    if verbose:
+        print "FINAL g_id:", g_id
+
+    # on détermine le sexe final du poisson
+    new_sex = None
+    if g_id < 0.666:
+        new_sex = "male"
+    elif g_id < 1.333:
+        new_sex = "intersexe"
+    elif g_id >= 1.333:
+        new_sex = "female"
     else:
-        return gender
+        print "ERROR: VALUE IMPOSSIBLE"
+
+    return new_sex
+
+
+
+
+    # Default for william
+    # # est-ce qu'il change de sexe ?
+    # elif 0.5 > np.random.uniform(0,1):
+    #     if gender == "male":
+    #         return "female"
+    #     else:
+    #         return "male"
+    # else:
+    #     return gender
+
 
 
 
